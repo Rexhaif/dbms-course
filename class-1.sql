fa9d20f96d84b39624429191035636432a984d4d
Daniil Larionov <rexhaif.io@gmail.com>
2019-11-21 14:19:59 +0300

    Update class-1.sql

--- class-1.sql
+++ class-1.sql
@@ -13,28 +13,28 @@ CREATE TABLE `surnames` (
 
 -- 2.Insert rows into these tables
 INSERT INTO `names` (`name`, `sex`) VALUES 
-(`Even`,        `m`),
-(`Isaak`,       `m`),
-(`Eskild`,      `m`),
-(`Noah`,        `m`),
-(`Joseph`,      `m`),
-(`Katherine`,   `f`),
-(`Elizabeth`,   `f`),
-(`Martha`,      `f`),
-(`Lilo`,        `f`),
-(`Miley`,       `f`);
+('Even',        'm'),
+('Isaak',       'm'),
+('Eskild',      'm'),
+('Noah',        'm'),
+('Joseph',      'm'),
+('Katherine',   'f'),
+('Elizabeth',   'f'),
+('Martha',      'f'),
+('Lilo',        'f'),
+('Miley',       'f');
 
 INSERT INTO `surnames` (`surname`, `sex`) VALUES 
-(`Naeshem`,     `m`),
-(`Valtersen`,   `m`),
-(`Moore`,       `m`),
-(`Reingard`,    `m`),
-(`Martin`,      `m`),
-(`Romanov`,     `f`),
-(`Tudor`,       `f`),
-(`Lin`,         `f`),
-(`Stich`,       `f`),
-(`Syrus`,       `f`);
+('Naeshem',     'm'),
+('Valtersen',   'm'),
+('Moore',       'm'),
+('Reingard',    'm'),
+('Martin',      'm'),
+('Romanov',     'f'),
+('Tudor',       'f'),
+('Lin',         'f'),
+('Stich',       'f'),
+('Syrus',       'f');
 
 -- 3. Join these tables
 DROP TABLE IF EXISTS `pairs`;
